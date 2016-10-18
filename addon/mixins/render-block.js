b6beb0e2443721a1fecd66795790cc79e3418025
Miguel Andrade <jmandrade0@gmail.com>
2016-10-18 23:07:29 +0100

    make destinationElement tag configurable

--- addon/mixins/render-block.js
+++ addon/mixins/render-block.js
@@ -6,8 +6,10 @@ const { Mixin, computed } = Ember;
 export default Mixin.create({
   layout,
 
+  destinationElementTag: 'div',
+
   // creates a document fragment that will hold the DOM
   destinationElement: computed(function() {
-    return document.createElement('div');
+    return document.createElement(this.get('wrapperElementTag'));
   })
 });
