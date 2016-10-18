57b5c6124c099db1dfdb55e8968d37a6fae0de20
Miguel Andrade <jmandrade0@gmail.com>
2016-10-18 23:17:36 +0100

    export mixins in index

---
+++ addon/index.js
@@ -0,0 +1,3 @@
+export { default as ChildMixin } from './mixins/child';
+export { default as ParentMixin } from './mixins/parent';
+export { default as RenderBlockMixin } from './mixins/render-block';
