1631800af692b15817cc39284398cac95ce8a013
David Pett <dpett@folklore.digital>
2017-01-03 12:38:37 -0600

    added fastboot guard

--- addon/mixins/render-block.js
+++ addon/mixins/render-block.js
@@ -1,15 +1,25 @@
 import Ember from 'ember';
 import layout from '../templates/render-block';
 
-const { Mixin, computed } = Ember;
+const { Mixin, computed, getOwner } = Ember;
 
 export default Mixin.create({
   layout,
+
+  fastboot: computed(function() {
+    let owner = getOwner(this);
+    return owner.lookup('service:fastboot');
+  }),
+  isFastBoot: computed('fastboot', function() {
+    return this.get('fastboot') && this.get('fastboot.isFastBoot');
+  }),
 
   destinationElementTag: 'div',
 
   // creates a document fragment that will hold the DOM
   destinationElement: computed(function() {
-    return document.createElement(this.get('destinationElementTag'));
+    if (!this.get('isFastBoot')) {
+      return document.createElement(this.get('destinationElementTag'));
+    }
   })
 });
