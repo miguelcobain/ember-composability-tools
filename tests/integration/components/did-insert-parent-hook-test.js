965fe9094c3b34931d2e1c092666529212a1ed31
Miguel Andrade <jmandrade0@gmail.com>
2024-03-03 01:58:40 +0000

    simplify tests

--- tests/integration/components/did-insert-parent-hook-test.js
+++ tests/integration/components/did-insert-parent-hook-test.js
@@ -2,137 +2,86 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
-import sinon from 'sinon';
 
 module(
   'Integration | Component | didInsertParent hook runs in the correct order',
   function (hooks) {
     setupRenderingTest(hooks);
 
+    hooks.beforeEach(function (assert) {
+      this.parentSpy = function () {
+        assert.step('parent');
+      };
+      this.childSpy = function () {
+        assert.step('child');
+      };
+      this.childParentSpy = function () {
+        assert.step('child-parent');
+      };
+    });
+
     test('top-level parent and two children', async function (assert) {
-      let parentSpy = (this.parentSpy = sinon.spy());
-      let childSpy = (this.childSpy = sinon.spy());
-
       await render(hbs`
-      <Root @didInsertParent={{this.parentSpy}} as |Node|>
-        <Node @didInsertParent={{this.childSpy}}/>
-        <Node @didInsertParent={{this.childSpy}}/>
-      </Root>
-    `);
+        <Root @didInsertParent={{this.parentSpy}} as |Node|>
+          <Node @didInsertParent={{this.childSpy}}/>
+          <Node @didInsertParent={{this.childSpy}}/>
+        </Root>
+      `);
 
-      assert.ok(parentSpy.calledOnce, 'parent didInsertParent was called once');
-      assert.ok(childSpy.calledTwice, 'child didInsertParent was called twice');
-      assert.ok(
-        parentSpy.calledBefore(childSpy),
-        'parent was called before child',
-      );
+      assert.verifySteps(['parent', 'child', 'child']);
     });
 
     test('top-level parent and two children after if', async function (assert) {
-      let parentSpy = (this.parentSpy = sinon.spy());
-      let childSpy = (this.childSpy = sinon.spy());
       this.show = false;
 
       await render(hbs`
-      <Root @didInsertParent={{this.parentSpy}} as |Node|>
-        {{#if this.show}}
-          <Node @didInsertParent={{this.childSpy}}/>
-          <Node @didInsertParent={{this.childSpy}}/>
-        {{/if}}
-      </Root>
-    `);
+        <Root @didInsertParent={{this.parentSpy}} as |Node|>
+          {{#if this.show}}
+            <Node @didInsertParent={{this.childSpy}}/>
+            <Node @didInsertParent={{this.childSpy}}/>
+          {{/if}}
+        </Root>
+      `);
 
-      assert.ok(parentSpy.calledOnce, 'parent didInsertParent was called once');
-      assert.notOk(childSpy.called, 'child didInsertParent was never called');
+      assert.verifySteps(['parent']);
 
       this.set('show', true);
 
-      assert.ok(parentSpy.calledOnce, 'parent didInsertParent was called once');
-      assert.ok(childSpy.calledTwice, 'child didInsertParent was called twice');
-      assert.ok(
-        parentSpy.calledBefore(childSpy),
-        'parent was called before child',
-      );
+      assert.verifySteps(['child', 'child']);
     });
 
     test('top-level parent and two children-parents', async function (assert) {
-      let parentSpy = (this.parentSpy = sinon.spy());
-      let childSpy = (this.childSpy = sinon.spy());
-      let childParentSpy = (this.childParentSpy = sinon.spy());
-
       await render(hbs`
-      <Root @didInsertParent={{this.parentSpy}} as |NodeA|>
-        <NodeA @didInsertParent={{this.childParentSpy}} as |NodeB|>
-          <NodeB @didInsertParent={{this.childSpy}}/>
-          <NodeB @didInsertParent={{this.childSpy}}/>
-        </NodeA>
-      </Root>
-    `);
-
-      assert.ok(parentSpy.calledOnce, 'parent didInsertParent was called once');
-      assert.ok(
-        childParentSpy.calledOnce,
-        'child-parent didInsertParent was called once',
-      );
-      assert.ok(childSpy.calledTwice, 'child didInsertParent was called twice');
-      assert.ok(
-        parentSpy.calledBefore(childParentSpy),
-        'parent was called before child-parent',
-      );
-      assert.ok(
-        childParentSpy.calledBefore(childSpy),
-        'child-parent was called before child',
-      );
-      assert.ok(
-        parentSpy.calledBefore(childSpy),
-        'parent was called before child',
-      );
-    });
-
-    test('top-level parent and two children-parents after if', async function (assert) {
-      let parentSpy = (this.parentSpy = sinon.spy());
-      let childSpy = (this.childSpy = sinon.spy());
-      let childParentSpy = (this.childParentSpy = sinon.spy());
-      this.show = false;
-
-      await render(hbs`
-      <Root @didInsertParent={{this.parentSpy}} as |NodeA|>
-        {{#if this.show}}
+        <Root @didInsertParent={{this.parentSpy}} as |NodeA|>
           <NodeA @didInsertParent={{this.childParentSpy}} as |NodeB|>
             <NodeB @didInsertParent={{this.childSpy}}/>
             <NodeB @didInsertParent={{this.childSpy}}/>
           </NodeA>
-        {{/if}}
-      </Root>
-    `);
+        </Root>
+      `);
 
-      assert.ok(parentSpy.calledOnce, 'parent didInsertParent was called once');
-      assert.notOk(
-        childParentSpy.called,
-        'child-parent didInsertParent was never called',
-      );
-      assert.notOk(childSpy.called, 'child didInsertParent was never called');
+      assert.verifySteps(['parent', 'child-parent', 'child', 'child']);
+    });
+
+    test('top-level parent and two children-parents after if', async function (assert) {
+      this.show = false;
+
+      await render(hbs`
+        <Root @didInsertParent={{this.parentSpy}} as |NodeA|>
+          {{#if this.show}}
+            <NodeA @didInsertParent={{this.childParentSpy}} as |NodeB|>
+              <NodeB @didInsertParent={{this.childSpy}}/>
+              <NodeB @didInsertParent={{this.childSpy}}/>
+            </NodeA>
+          {{/if}}
+        </Root>
+      `);
+
+      assert.verifySteps(['parent']);
 
       this.set('show', true);
 
-      assert.ok(parentSpy.calledOnce, 'parent didInsertParent was called once');
-      assert.ok(
-        childParentSpy.calledOnce,
-        'child-parent didInsertParent was called once',
-      );
-      assert.ok(childSpy.calledTwice, 'child didInsertParent was called twice');
-      assert.ok(
-        parentSpy.calledBefore(childParentSpy),
-        'parent was called before child-parent',
-      );
-      assert.ok(
-        childParentSpy.calledBefore(childSpy),
-        'child-parent was called before child',
-      );
-      assert.ok(
-        parentSpy.calledBefore(childSpy),
-        'parent was called before child',
-      );
+      assert.verifySteps(['child-parent', 'child', 'child']);
     });
   },
 );
--- tests/integration/components/will-destroy-parent-hook-test.js
+++ tests/integration/components/will-destroy-parent-hook-test.js
@@ -2,126 +2,82 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
-import sinon from 'sinon';
 
 module(
   'Integration | Component | willDestroyParent hook runs in the correct order',
   function (hooks) {
     setupRenderingTest(hooks);
 
+    hooks.beforeEach(function (assert) {
+      this.parentSpy = function () {
+        assert.step('parent');
+      };
+      this.childSpy = function () {
+        assert.step('child');
+      };
+      this.childParentSpy = function () {
+        assert.step('child-parent');
+      };
+    });
+
     test('top-level parent and two children', async function (assert) {
-      let parentSpy = (this.parentSpy = sinon.spy());
-      let childSpy = (this.childSpy = sinon.spy());
       this.show = true;
 
       await render(hbs`
-      {{#if this.show}}
-        <Root @willDestroyParent={{this.parentSpy}} as |Node|>
-          <Node @willDestroyParent={{this.childSpy}}/>
-          <Node @willDestroyParent={{this.childSpy}}/>
-        </Root>
-      {{/if}}
-    `);
+        {{#if this.show}}
+          <Root @willDestroyParent={{this.parentSpy}} as |Node|>
+            <Node @willDestroyParent={{this.childSpy}}/>
+            <Node @willDestroyParent={{this.childSpy}}/>
+          </Root>
+        {{/if}}
+      `);
 
       this.set('show', false);
 
-      assert.ok(
-        parentSpy.calledOnce,
-        'parent willDestroyParent was called once',
-      );
-      assert.ok(
-        childSpy.calledTwice,
-        'child willDestroyParent was called twice',
-      );
-      assert.ok(
-        childSpy.calledBefore(parentSpy),
-        'child was called before parent',
-      );
+      assert.verifySteps(['child', 'child', 'parent']);
     });
 
     test('top-level parent and two children after if', async function (assert) {
-      let parentSpy = (this.parentSpy = sinon.spy());
-      let childSpy = (this.childSpy = sinon.spy());
-      let childParentSpy = (this.childParentSpy = sinon.spy());
       this.showChild = this.showParentChild = this.show = true;
 
       await render(hbs`
-      {{#if this.show}}
-        <Root @willDestroyParent={{this.parentSpy}} as |Node|>
-          <Node @willDestroyParent={{this.childSpy}}/>
-          <Node @willDestroyParent={{this.childSpy}}/>
-          {{#if this.showParentChild}}
-            <Node @willDestroyParent={{this.childParentSpy}} as |Node|>
-              {{#if this.showChild}}
+        {{#if this.show}}
+          <Root @willDestroyParent={{this.parentSpy}} as |Node|>
+            <Node @willDestroyParent={{this.childSpy}}/>
+            <Node @willDestroyParent={{this.childSpy}}/>
+            {{#if this.showParentChild}}
+              <Node @willDestroyParent={{this.childParentSpy}} as |Node|>
+                {{#if this.showChild}}
+                  <Node @willDestroyParent={{this.childSpy}}/>
+                {{/if}}
                 <Node @willDestroyParent={{this.childSpy}}/>
-              {{/if}}
-              <Node @willDestroyParent={{this.childSpy}}/>
-            </Node>
-          {{/if}}
-        </Root>
-      {{/if}}
-    `);
+              </Node>
+            {{/if}}
+          </Root>
+        {{/if}}
+      `);
 
       this.set('showChild', false);
 
-      assert.notOk(
-        parentSpy.called,
-        'parent willDestroyParent was never called',
-      );
-      assert.notOk(
-        childParentSpy.called,
-        'child-parent willDestroyParent was never called',
-      );
-      assert.ok(childSpy.calledOnce, 'child willDestroyParent was called once');
+      assert.verifySteps(['child']);
 
-      childSpy.resetHistory();
       this.set('showChild', true);
       this.set('showParentChild', false);
 
-      assert.notOk(
-        parentSpy.called,
-        'parent willDestroyParent was never called',
-      );
-      assert.ok(
-        childParentSpy.calledOnce,
-        'child-parent willDestroyParent was called once',
-      );
-      assert.ok(
-        childSpy.calledTwice,
-        'child willDestroyParent was called twice',
-      );
-      assert.ok(
-        childSpy.calledBefore(childParentSpy),
-        'child was called before child-parent',
-      );
+      assert.verifySteps(['child', 'child', 'child-parent']);
 
-      childSpy.resetHistory();
-      childParentSpy.resetHistory();
       this.set('showChild', true);
       this.set('showParentChild', true);
       this.set('show', false);
 
-      assert.ok(
-        parentSpy.calledOnce,
-        'parent willDestroyParent was called once',
-      );
-      assert.ok(
-        childParentSpy.calledOnce,
-        'child-parent willDestroyParent was called once',
-      );
-      assert.strictEqual(
-        childSpy.callCount,
-        4,
-        'child willDestroyParent was called 4 times',
-      );
-      assert.ok(
-        childSpy.calledBefore(childParentSpy),
-        'child was called before child-parent',
-      );
-      assert.ok(
-        childParentSpy.calledBefore(parentSpy),
-        'child-parent was called before parent',
-      );
+      assert.verifySteps([
+        'child',
+        'child',
+        'child',
+        'child',
+        'child-parent',
+        'parent',
+      ]);
     });
   },
 );
